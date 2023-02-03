b7e8c8d82ee242225c7174a28ed7958430f0eaff
mille055 <46400739+mille055@users.noreply.github.com>
2023-02-02 19:04:08 -0500

    adding files from my fork
    Adding finger math .py files and pics of barnyard animal toys

---
+++ test_finger.py
@@ -0,0 +1,41 @@
+import mediapipe as mp 
+import numpy as np
+import cv2
+from random import randrange
+from finger_counter_webcam2 import get_finger_count
+
+
+def get_addition_prob():
+  x = randrange(10)
+  y = randrange(10)
+  answer = max(x,y)
+  addend1 = min(x,y)
+  addend2 = answer-addend1
+  
+  assert addend1+addend2 == answer
+  assert answer<11
+  
+  return addend1, addend2, answer
+
+num_correct = 0
+num_probs = 5
+for prob in range(num_probs):
+  addend1, addend2, answer = get_addition_prob()
+  question_string = f'{addend1}+{addend2}='
+  print(question_string)
+  
+  
+  my_answer = get_finger_count(countdown = 10, question_string = question_string)
+  
+  if my_answer == answer:
+    print('Way to go! That is correct!')
+    num_correct = num_correct + 1
+
+  elif np.abs(my_answer-answer) == 1:
+    print('So close!')
+  else:
+    print('Incorrect.')
+ 
+print(f'You got {num_correct} out of {num_probs} correct!')
+
+  
