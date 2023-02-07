e9cbb5eb285c4edec20cc877a629dadd27f137fd
mille055 <46400739+mille055@users.noreply.github.com>
2023-02-07 18:53:30 -0500

    fixing typo

--- train_transfer_classifier.py
+++ train_transfer_classifier.py
@@ -45,7 +45,7 @@ data_transforms = {
 		transforms.CenterCrop(224),
         transforms.RandomHorizontalFlip(),
 		transforms.RandomVerticalFlip(),
-	    transforms.ColorJitter(brightness=(0.0,1,5), contrast=(1))
+	    transforms.ColorJitter(brightness=(0.0,1,5), contrast=(1)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
