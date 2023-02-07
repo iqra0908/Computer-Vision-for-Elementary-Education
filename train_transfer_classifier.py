5239f9c7d7f5771e1358212a781a80b9aa7560e4
mille055 <46400739+mille055@users.noreply.github.com>
2023-02-07 18:58:13 -0500

    fixing typos

--- train_transfer_classifier.py
+++ train_transfer_classifier.py
@@ -60,6 +60,7 @@ data_transforms = {
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 }
 
 # Create Datasets for training and validation sets
