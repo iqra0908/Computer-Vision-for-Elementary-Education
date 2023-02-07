91ab6cffa96d7ca0a05ec1c7ba54ad2f1b235f69
mille055 <46400739+mille055@users.noreply.github.com>
2023-02-07 14:50:58 -0500

    adding comments
    comments for readability and for streamlit incorporation

--- test_finger.py
+++ test_finger.py
@@ -1,3 +1,14 @@
+#### Finger counter using webcam to answer simple arithmetic problems
+#### Heavily borrows from mediapipe website explaining mediapipe at https://google.github.io/mediapipe/solutions/hands.html
+#### to link with streamlit, some values that could be sliders/buttons include:
+####       num_probs = number of problems
+####.      which_type = type of problems, including 'addition', 'subtraction' or 'mixed' (although anything not addition or subtraction = mixed
+####       countdown = time in seconds to allow for answer of each problem
+
+#### Note: still need exit key to stop the math test...ESC exits the webcam for each problem, but will still cycle through to num_probs
+
+
+
 import mediapipe as mp 
 import numpy as np
 import cv2
@@ -9,7 +20,7 @@ num_probs = 5
 which_type = 'addition'
 countdown = 10
 
-
+# Function to come up with random addition problem, making sure the answer is 10 or less
 def get_addition_prob():
   x = randrange(10)
   y = randrange(10)
@@ -24,6 +35,7 @@ def get_addition_prob():
   
   return question_string, answer
 
+# Function to come up with random subtraction proble making sure the answer is between 0 and 10
 def get_subtraction_prob():
   x = randrange(10)
   y = randrange(10)
@@ -55,7 +67,7 @@ for prob in range(num_probs):
     else: 
       question_string, answer = get_subtraction_prob()
    
-  #get the answer from the webcam module
+  #get the answer from the webcam module, sends the question string to display on webcam
   my_answer = get_finger_count(countdown = countdown, question_string = question_string)
   
   #compare given answer with correct answer
@@ -67,7 +79,8 @@ for prob in range(num_probs):
      print('So close!')
   else:
     print('Incorrect.')
- 
+
+# this and the feedback from each question could be output to streamlit not currently on the webcam display
 print(f'You got {num_correct} out of {num_probs} correct!')
 
   
